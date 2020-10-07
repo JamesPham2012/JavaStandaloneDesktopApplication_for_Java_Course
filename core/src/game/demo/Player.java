3e940ae9236541f13eab167753b1fe0bb4284646
bipowerhcmcity <lmht772000@gmail.com>
2020-10-07 13:40:27 +0700

    Khoa Update

---
+++ core/src/game/demo/Player.java
@@ -0,0 +1,52 @@
+package game.demo;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+public class Player{
+    private SpriteBatch batch;
+    private Texture texture;
+    private int x;
+    private int y;
+
+
+
+    public void create(){
+
+        batch = new SpriteBatch();
+        texture = new Texture("plane.png");
+    }
+    public void input(){
+        x= Gdx.input.getX();
+        y= 480 - Gdx.input.getY();
+
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public boolean fire(){
+        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void render_player () { // loop
+        batch.begin();
+        batch.draw(texture, x, y);
+        batch.end();
+        input();
+
+    }
+    public void dispose () {
+        batch.dispose();
+        texture.dispose();
+    }
+}
