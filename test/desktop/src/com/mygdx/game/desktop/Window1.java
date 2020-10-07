8d601c470280700e1a7501210de33f1d2118e1d7
bipowerhcmcity <lmht772000@gmail.com>
2020-10-07 18:23:45 +0700

    window1

---
+++ test/desktop/src/com/mygdx/game/desktop/Window1.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.desktop;
+
+import javax.swing.*;
+
+public class Window1 {
+    private JPanel panel1;
+    private JFormattedTextField formattedTextField1;
+    private JButton submitButton;
+    private JPasswordField passwordField1;
+
+    public void enable(){
+        panel1.setVisible(true);
+        formattedTextField1.setVisible(true);
+        submitButton.setVisible(true);
+        passwordField1.setVisible(true);
+    }
+}
