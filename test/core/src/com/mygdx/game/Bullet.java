0a18632ec1e6639902e6b37c1ce36380880602af
bipowerhcmcity <lmht772000@gmail.com>
2020-10-07 14:32:06 +0700

    Tui chen file vao file cua hao

---
+++ test/core/src/com/mygdx/game/Bullet.java
@@ -0,0 +1,37 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Bullet {
+    private SpriteBatch batch;
+    private Texture texture;
+    private int x;
+    private int y;
+
+
+    public Bullet(int x_c, int y_c){
+        x=x_c;
+        y=y_c;
+    }
+    public void create(){
+
+        batch = new SpriteBatch();
+        texture = new Texture("Bullet_plane.png");
+    }
+    public void render () { // loop
+        batch.begin();
+        batch.draw(texture, x, y);
+        batch.end();
+        y++;
+    }
+
+    public void dispose () {
+        batch.dispose();
+        texture.dispose();
+    }
+}
--- test/desktop/src/game/demo/desktop/DesktopLauncher.java
+++ test/desktop/src/game/demo/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package game.demo.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import game.demo.MyGdxGame;
+import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
