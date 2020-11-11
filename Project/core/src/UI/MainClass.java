80c1c5807ba1deff9e23ea17584c08e5e1718a08
VGUlCS2018Uniform <52058856+VGUlCS2018Uniform@users.noreply.github.com>
2020-11-12 02:34:12 +0700

    Frame example for extra audio files

--- Project/core/src/UI/MainClass.java
+++ Project/core/src/UI/MainClass.java
@@ -1,12 +1,14 @@
 package UI;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.audio.Music;
 import game.demo.Multiplayer.MultiplayerGame;
 import game.demo.MyGdxGame;
 import UI.PlayerData;
 
 public class MainClass extends Game implements ApplicationListener {
-
+    private Music BGMMM;
+    private Music BGMIG;
     private MyGdxGame gameScreen;
     private MainMenu mainMenu;
     private PauseScreen pauseScreen;
@@ -20,6 +22,10 @@ public class MainClass extends Game implements ApplicationListener {
 
     @Override
     public void create() {
+        BGMMM = Gdx.audio.newMusic(Gdx.files.internal("Audio/BGM1.wav"));
+        BGMIG = Gdx.audio.newMusic(Gdx.files.internal("Audio/BGMIG.mp3"));
+        BGMIG.setVolume(0.1f);
+        BGMMM.setVolume(0.25f);
         gameScreen=new MyGdxGame(this);
 
         mainMenu = new MainMenu(this);
@@ -35,28 +41,37 @@ public class MainClass extends Game implements ApplicationListener {
     public void setNewGameScreen(){
         gameScreen=new MyGdxGame(this);
         setScreen(gameScreen);
+        BGMMM.stop();
+        BGMIG.play();
     }
 
     public void setMenuScreen()
     {
         setScreen(mainMenu);
         gameScreen.pause();
+        BGMMM.play();
+        BGMIG.stop();
 
     }
     public void setMultiplayerScreen(){
         multiplayerScreen = new MultiplayerGame(this);
         setScreen(multiplayerScreen);
+        BGMMM.stop();
+        BGMIG.play();
     }
     public void setPauseScreen()
     {
         setScreen(pauseScreen);
         gameScreen.pause();
+        BGMMM.play();
+        BGMIG.stop();
     }
     public void setGameOverScreen()
     {
         score = gameOver.getScore();
         setScreen(gameOver);
         gameScreen.pause();
+
     }
     public void setScoreBoardScreen()
     {
